d6a4beba09e1cc4154d9122b59a73224db089d30
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-09-15 22:43:03 +0530

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -1,1 +1,19 @@
 
+import collections
+def breadth_first_search(graph, root) : 
+    visited, queue = set(), collections.deque([root])
+    visited.add(root)
+
+    while queue : 
+        vertex = queue.popleft()
+        print(str(vertex) + " ", end = "")
+
+        for neighbour in graph[vertex] :
+            if neighbour not in visited : 
+                visited.add(neighbour)
+                queue.append(neighbour)
+
+if __name__ == '__main__' : 
+    graph = {0 : [1, 2], 1 : [3], 2 : [4], 3 : [1, 2], 4 : [2, 3]}
+    print("The Breadth First Search is : ")
+    breadth_first_search(graph, 0) 
