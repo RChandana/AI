592ffce7641dbd9f9eb4414ee449b25d473e3aa1
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-06 21:00:42 +0530

    Update 8_Puzzle.py

--- 8_Puzzle.py
+++ 8_Puzzle.py
@@ -1,1 +1,18 @@
-
+import copy   
+from heapq import heappush, heappop  
+n = 3  
+rows = [ 1, 0, -1, 0 ]  
+cols = [ 0, -1, 0, 1 ]  
+class priorityQueue:  
+    def __init__(self):  
+        self.heap = []  
+    def push(self, key):  
+        heappush(self.heap, key)  
+    def pop(self):  
+        return heappop(self.heap)  
+    def empty(self):  
+        if not self.heap:  
+            return True  
+        else:  
+            return False  
+class nodes:  
