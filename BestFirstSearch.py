2c64d0bb6de038ef29df2ae5ca34e26d7959fbc8
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-07 18:22:49 +0530

    Update BestFirstSearch.py

--- BestFirstSearch.py
+++ BestFirstSearch.py
@@ -23,3 +23,24 @@ def best_first_search(actual_Src, target, n) :
 def add_edge(x, y, cost) :
     graph[x].append((y, cost))      
     graph[y].append((x, cost))       
+add_edge(0, 1, 4)
+add_edge(0, 2, 8)
+add_edge(0, 4, 5)
+add_edge(1, 3, 9)
+add_edge(1, 5, 6)
+add_edge(2, 8, 12)
+add_edge(2, 7, 14)
+add_edge(4, 6, 7)
+add_edge(6, 9, 5)
+add_edge(6, 10, 8)
+add_edge(9, 11, 1)
+add_edge(9, 12, 10)
+add_edge(9, 13, 2)
+
+start_node = 0
+goal_node = 13
+
+best_first_search(start_node, goal_node, nodes)
+print("\nThe initial node is : " , start_node )
+print("\nThe goal node is : " , goal_node)
+print("\nHence this algorithm is Best First Algorithm!!")
