4adbb39da3f2d41f765097af9f96fc823e67e23c
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-07 18:22:01 +0530

    Update BestFirstSearch.py

--- BestFirstSearch.py
+++ BestFirstSearch.py
@@ -1,1 +1,25 @@
+from queue import PriorityQueue
+from re import T
+nodes = 14
+graph = [[] for i in range(nodes)]
 
+def best_first_search(actual_Src, target, n) : 
+    visited = [False] * n
+    priority_queue = PriorityQueue()
+    priority_queue.put((0, actual_Src))
+    visited[actual_Src] = True
+
+    while priority_queue.empty() == False : 
+        a = priority_queue.get()[1]
+        print(a, end = " ")
+        if a == target : 
+            break
+        for v, c in graph[a] : 
+            if visited[v] == False : 
+                visited[v] = True
+                priority_queue.put((c, v))
+    print()
+
+def add_edge(x, y, cost) :
+    graph[x].append((y, cost))      
+    graph[y].append((x, cost))       
