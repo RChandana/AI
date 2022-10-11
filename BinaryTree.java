61638b8a8d4e174e7fdd78792a4a7b10c3274652
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-11 18:02:44 +0530

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,1 +1,21 @@
+import java.util.LinkedList;  
+import java.util.Queue;  
+   
+public class BinaryTree {  
 
+      public static class Node{  
+        int data;  
+        Node left;  
+        Node right;  
+          
+        public Node(int data){  
+          this.data = data;  
+          this.left = null;  
+          this.right = null;  
+        }  
+      }  
+      public Node root;  
+        
+      public BinaryTree(){  
+        root = null;  
+      }
