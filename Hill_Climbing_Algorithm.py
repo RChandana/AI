bbdb37a3f9b477e6996d8e7dcf9e4e84cad949dd
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-20 11:56:09 +0530

    Update Hill_Climbing_Algorithm.py

--- Hill_Climbing_Algorithm.py
+++ Hill_Climbing_Algorithm.py
@@ -1,1 +1,13 @@
 # This code uses Travelling Salesman Problem to solve the Hill Climbing Algorithm
+
+import random
+def random_Solution(travelling_salesman_problem):
+    cities = list(range(len(travelling_salesman_problem)))
+    solution = []
+
+    for i in range(len(travelling_salesman_problem)):
+        random_City = cities[random.randint(0, len(cities) - 1)]
+        solution.append(random_City)
+        cities.remove(random_City)
+
+    return solution
