27c6a7637f1d6f2ad5eed95ec6d925e688218e96
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-20 11:51:45 +0530

    Update A_Star_Algorithm.py

--- A_Star_Algorithm.py
+++ A_Star_Algorithm.py
@@ -1,1 +1,22 @@
+from collections import deque
+class Graph:
+    def __init__(self, map):
+        self.map = map
 
+    def get_neighbors(self, v):
+        return self.map[v]
+
+    def h(self, n):
+        H = {
+            'A': 11,
+            'B': 6,
+            'C': 5,
+            'D': 7,
+            'E': 3,
+            'F': 6,
+            'G': 5,
+            'H': 3,
+            'I': 1,
+            'J': 0
+        }
+        return H[n]
