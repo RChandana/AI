fe3359a598f0dacc4025a5e1a1117290713edbcc
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-20 12:01:00 +0530

    Update Comparison_between_BFS_and_DFS.py

--- Comparison_between_BFS_and_DFS.py
+++ Comparison_between_BFS_and_DFS.py
@@ -15,3 +15,22 @@ graph = {
     'L': [],
     'M': []
 }
+
+def breadth_first_search(visit_complete, graph, current_node):
+    visit_complete.append(current_node)
+    queue = [current_node]
+
+    while queue:
+        s = queue.pop(0)
+        print(s)
+
+        for neighbour in graph[s]:
+            if neighbour not in visit_complete:
+                visit_complete.append(neighbour)
+                queue.append(neighbour)
+
+
+print("Breadth first search algorithm: ")
+breadth_first_search([], graph, 'A')
+
+visited = set()
