5ad6ddf9fb049f763ffa6561e623027a1143d2b9
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-20 12:00:44 +0530

    Update Comparison_between_BFS_and_DFS.py

--- Comparison_between_BFS_and_DFS.py
+++ Comparison_between_BFS_and_DFS.py
@@ -1,1 +1,17 @@
 # This code is for comparing between BFS and DFS Search Algorithms
+
+graph = {
+    'A': ['C', 'B'],
+    'B': ['F', "G"],
+    'C': ['D', "E"],
+    'D': [],
+    'E': ['I', 'H'],
+    "F": [],
+    'G': [],
+    "H": [],
+    'I': ['J', 'K'],
+    'J': [],
+    'K': ['L', 'M'],
+    'L': [],
+    'M': []
+}
