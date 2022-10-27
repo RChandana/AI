b7ae127b5a07e579a38b40c53f891eda3509725b
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-10-27 12:14:54 +0530

    Update 8_Queen_Problem.py

--- 8_Queen_Problem.py
+++ 8_Queen_Problem.py
@@ -1,1 +1,12 @@
+from __future__ import print_function
+N = 8
+left_diagonal = [0] * 30
+right_diagonal = [0] * 30
+column = [0] * 30
 
+def print_Solution(board):
+	for i in range(N):
+		for j in range(N):
+			print(board[i][j], end = " ")
+		print()
+ 
