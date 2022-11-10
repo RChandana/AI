e98d9eef6f3a96e225770c376ac806f8eb64ed84
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-11-10 23:27:59 +0530

    Update Uniform_Cost_Search.py

--- Uniform_Cost_Search.py
+++ Uniform_Cost_Search.py
@@ -1,1 +1,8 @@
-
+import queue as Q
+def search(graph, start, end):
+    if start not in graph:
+        raise TypeError(str(start) + ' not found in graph !')
+        return
+    if end not in graph:
+        raise TypeError(str(end) + ' not found in graph !')
+        return
