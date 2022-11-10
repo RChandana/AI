2baafe140a4ddcaffdd3f51e630f63571f1d0b48
Chandana <chandana.21bce8718@vitapstudent.ac.in>
2022-11-10 23:28:18 +0530

    Update Uniform_Cost_Search.py

--- Uniform_Cost_Search.py
+++ Uniform_Cost_Search.py
@@ -6,3 +6,20 @@ def search(graph, start, end):
     if end not in graph:
         raise TypeError(str(end) + ' not found in graph !')
         return
+    
+    queue = Q.PriorityQueue()
+    queue.put((0, [start]))
+    
+    while not queue.empty():
+        node = queue.get()
+        current = node[1][len(node[1]) - 1]
+        
+        if end in node[1]:
+            print("Path found: " + str(node[1]) + ", Cost = " + str(node[0]))
+            break
+        
+        cost = node[0]
+        for neighbor in graph[current]:
+            temp = node[1][:]
+            temp.append(neighbor)
+            queue.put((cost + graph[current][neighbor], temp))
